86a2d78c632f9386a3a844252b691d03cf5d4c22
ivanberry <tabvim.dev@gmail.com>
2017-07-04 05:38:48 +0800

    [Update]:handle user input

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,37 @@ class App extends Component {
 
     handleFormSubmit(event) {
         event.preventDefault();
-        console.log('form submit!');
+        const formType = window.location.href.split('/').reverse()[0];
+        let data;
+        if (formType === 'login') {
+            data = {
+                email: this.state.formData.email,
+                password: this.state.formData.password
+            }
+        }
+        if (formType === 'register') {
+            data = {
+                username: this.state.formData.username,
+                email: this.state.formData.email,
+                password: this.state.formData.password
+            }
+        }
+
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`;
+        axios.post(url, data)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    formData: {
+                        username: '',
+                        email: '',
+                        password: ''
+                    }
+                });
+        })
+            .catch((err) => {
+                console.log(err);
+        })
     }
 
     getUsers() {
