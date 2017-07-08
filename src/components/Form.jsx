e57d6f51cb6d843d88248a0916bda1aa2cdf0add
ivanberry <tabvim.dev@gmail.com>
2017-07-08 16:22:23 +0800

    [Update]: form component to class component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,55 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 
-const Form = (props) => {
-    if (props.isAuthenticated) {
-     return <Redirect to='/' />
-    }    
-    return (
-        <div>
-            <h1>{props.formType}</h1>
-            <hr /><br />
-            <form onSubmit={(event) => props.handleUserFromSubmit(event)}>
-                {/*//这是什么鬼？*/}
-                {props.formType === 'Register' &&
+class Form extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+        if (this.props.isAuthenticated) {
+            return <Redirect to='/' />
+        }
+        return (
+            <div>
+                <h1>{this.props.formType}</h1>
+                <hr /><br />
+                <form onSubmit={(event) => this.props.handleUserFromSubmit(event)}>
+                    {/*//这是什么鬼？*/}
+                    {this.props.formType === 'Register' &&
+                        <div>
+                            <input
+                                name='username'
+                                type='text'
+                                placeholder='Enter a username'
+                                required
+                                value={this.props.formData.username}
+                                onChange={this.props.handleFormChange}
+                            />
+                        </div>
+                    }
                     <div>
                         <input
-                            name='username'
-                            type='text'
-                            placeholder='Enter a username'
+                            name='email'
+                            type="email"
+                            placeholder='Enter your email'
                             required
-                            value={props.formData.username}
-                            onChange={props.handleFormChange}
+                            value={this.props.formData.email}
+                            onChange={this.props.handleFormChange}
                         />
                     </div>
-                }
-                <div>
+                    <div>
+                        <input
+                            name='password'
+                            type="password"
+                            placeholder='Enter your password'
+                            required
+                            value={this.props.formData.password}
+                            onChange={this.props.handleFormChange}
+                        />
+                    </div>
                     <input
-                        name='email'
-                        type="email"
-                        placeholder='Enter your email'
-                        required
-                        value={props.formData.email}
-                        onChange={props.handleFormChange}
+                        type="submit"
+                        value='Submit'
                     />
-                </div>
-                <div>
-                    <input
-                        name='password'
-                        type="password"
-                        placeholder='Enter your password'
-                        required
-                        value={props.formData.password}
-                        onChange={props.handleFormChange}
-                    />
-                </div>
-                <input
-                    type="submit"
-                    value='Submit'
-                />
-            </form>
-        </div>
-    )
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Form
