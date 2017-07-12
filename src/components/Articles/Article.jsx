c5ebe6ece74b9d2ae4aa7e15dba0aeeccf692ae2
ivanberry <tabvim.dev@gmail.com>
2017-07-12 13:21:04 +0800

    [Update]: Article with Editor

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -1,47 +1,23 @@
 import React, { Component } from 'react';
+import Editor from '../Common/Editor';
 
 class Article extends Component {
     //组件内包含两种状态：编辑和预览
     constructor() {
         super();
         this.state = {
-            article: '',
-            preview: false
+            article: ''
         }
     }
 
     componentDidMount() {
-        // this.renderMark();
-    }
 
-    renderMark(value) {
-        let marked = require('marked');
-        let _article = marked(value);
-        this.setState({
-            article: _article
-        });
     }
-
-    // 输入即时展示，监听onChange事件
-    handlerTextChange(event) {
-        let value = event.target.value;
-        this.renderMark(value);
-    }
+    
 
     render() {
         return (
-            <main>
-                <section>
-                    <h1>Add New Article</h1>
-                    <div>
-                        <button>编辑</button>
-                        <button>预览</button>
-                    </div>
-                    <textarea required rows='20' cols='100' placeholder='Hi, do some write?'  onChange={this.handlerTextChange.bind(this)}></textarea>
-                    <button>提交</button>
-                </section>
-                <section dangerouslySetInnerHTML={{__html: this.state.article}}></section>
-            </main>
+            <Editor />
         )
     }
 
