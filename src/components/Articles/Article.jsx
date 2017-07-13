3a29f4300b57478cf4f50275f4dac288d23acbf6
ivanberry <tabvim.dev@gmail.com>
2017-07-13 22:21:29 +0800

    [Add]: Article status between user\'s artciles and create new one

--- src/components/Articles/Article.jsx
+++ src/components/Articles/Article.jsx
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
 import Editor from '../Common/Editor';
 
+import axios from 'axios';
+
 class Article extends Component {
-    //组件内包含两种状态：编辑和预览
+    //get user's all articles or create new one
     constructor(props) {
         super(props);
         this.state = {
-            article: ''
+            status: true,
+            articles: []
         }
+    }
+
+    componentDidMount() {
+        axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/articles`)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    articles: res.data.data.articles
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    handlerUserClick() {
+        this.setState({
+            status: !this.state.status
+        });
     }
 
     render() {
         return (
-            <Editor
-                isAuthenticated={this.props.isAuthenticated}
-            />
+            <div>
+                <button onClick={this.handlerUserClick.bind(this)}>Create New Article</button>
+                {this.state.status &&
+                    <main>
+                        {
+                            this.state.articles.map((article) => {
+                                return (
+                                    <section>
+                                        <h1>{article.titcle}</h1>
+                                        <div>{article.body}</div>
+                                        <br/>
+                                        <p>{article.pub_at}</p>
+                                    </section>
+                                )
+                            })
+                        }
+                    </main>
+                }
+                {!this.state.status &&
+                    <Editor
+                        isAuthenticated={this.props.isAuthenticated}
+                    />
+                }
+            </div>
         )
     }
 
